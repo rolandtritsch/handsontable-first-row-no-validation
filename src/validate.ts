6f0558eace3b4e7d8f9d0a49ad9613bc33c8eee1
Roland Tritsch <roland@tritsch.email>
2023-06-05 07:37:10 +0100

    Simplify test-case.

--- src/validate.ts
+++ src/validate.ts
@@ -25,7 +25,7 @@ function loadRows(reader: FileReader): void {
 }
 
 sheet.addHook('afterLoadData', (sourceData, _initialLoad, _source) => {
-  const allRows = Array.from({ length: sourceData.length - 1 }, (_, i) => i + 1)
+  const allRows = Array.from(Array(sourceData.length).keys())
   sheet.validateRows(allRows)
 })
 
@@ -39,10 +39,10 @@ sheet.addHook('afterValidate', (isValid, _value, row, _prop) => {
  * Turn a csv-string into and 2D array.
  * @return {string[][]} All the rows/cols (as an 2D array).
  */
-// We need this dummy/first row to work around a bug in handsontable
-// const dummyRow = '11231231234,01/01/1970,Dummy,Bug,01/01/1970,non-binary,NA,10001,NA,CA,Canada,CA,dummy.bug@gmail.com'
+// dummy/first row to work around the bug
+// const dummyRow = '11231231234,01/01/1970'
 function csvStrToArray(str: string, delimiter = ','): string[][] {
-  const rawRows = str.slice(str.indexOf('\n') + 1).split('\n')
+  const rawRows = str.split('\n')
   // rawRows.unshift(dummyRow)
   const rows = rawRows.map(function (rawRow) {
     const row = rawRow.split(delimiter)
